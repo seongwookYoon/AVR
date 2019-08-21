1cd92262e073e855306fc2fca2605764f392025c
Steve <swyoon@seloco.com>
2019-08-21 12:01:18 +0900

    modify UART_RX_ECho

--- ATmega64/ATmega64_UART/01_UART_Rx_Echo/01_UART_Rx_Echo/01_UART_Rx_Echo/01_UART_Rx_Echo.c
+++ ATmega64/ATmega64_UART/01_UART_Rx_Echo/01_UART_Rx_Echo/01_UART_Rx_Echo/01_UART_Rx_Echo.c
@@ -13,29 +13,55 @@
 #include <util/delay.h>
 #include <avr/interrupt.h>
 
+#define MAX_RX_NUM	3	//최대 수신 바이트 수
+//===========================================================================
+//                         STRUCT
+//===========================================================================
+
+//유아트 수신 구조체
+typedef struct uart1_rx_struct
+{
+	volatile unsigned char rcvFlag;	//수신 완료 플래그
+	volatile unsigned char rcvCnt;  //수신 횟수 카운터 
+	unsigned char rcvBuff[MAX_RX_NUM]; //수신 데이터 저장용 버퍼
+	
+}uart1_rx_type;
+//===========================================================================
+//                          GLOBAL VARIABLES
+//===========================================================================
+uart1_rx_type com1;	//유아트 수신 구조체 변수
+
+//===========================================================================
+//                          GLOBAL FUNCTIONS
+//===========================================================================
+
 //1Byte 송신 함수
 void USART_Transmit( unsigned char data )
 {
-
 	/* Wait for empty transmit buffer */
 	while ( !( UCSR1A & (1<<UDRE1)) )
 	;
-
 	/* Put data into buffer, sends the data */
 	UDR1 = data;
 }
 
 //nByte 송신 함수
-void USART_putString(unsigned char *str)
+/*void USART_putString(unsigned char *str)
 {
 	while(*str)
 	{
 		USART_Transmit(*str);
 		str++;
 	}
+}*/
+void USART_putString(unsigned char *str, unsigned char len)
+{
+	for(int i=0; i< len; i++)
+	{
+		USART_Transmit(*str);
+		str++;
+	}
 }
-
-
 
 // 1Byte 수신 함수
 unsigned char USART_Receive( void )
@@ -48,12 +74,9 @@ unsigned char USART_Receive( void )
 	return UDR1;
 }
 
-
-
 //UART 초기화 함수
 void USART_ini( unsigned int ubrr )
 {
-
 	/* Set baud rate */
 	UBRR1H = (unsigned char)(ubrr>>8);
 	UBRR1L = (unsigned char)ubrr;
@@ -64,37 +87,32 @@ void USART_ini( unsigned int ubrr )
 
 	/* Set frame format: 8data, 2stop bit */
 	UCSR1C = (1<<USBS1)|(3<<UCSZ10);
-
 }
 
-//UART1 Rx 인터럽트 서비스 루틴
 ISR(USART1_RX_vect)
-{
+{	
+	com1.rcvBuff[com1.rcvCnt++] = UDR1; //수신 데이터 버퍼에 저장
 	
+	if(com1.rcvCnt == MAX_RX_NUM) //현재 수신된 데이터 수와 최대 수신 바이트 수 비교
+	{
+		com1.rcvFlag = 1;	//수신 완료 플래그 설정
+		com1.rcvCnt=0;		//수신 데이터 카운트 초기화
+	}
 }
-
-
 
 int main(void)
-
-{
-
-	USART_ini(MYUBRR);            //UART초기화 함수 호출
-
-	unsigned char data = 0x5A;    //UART 송신 데이터
+{	
+	USART_ini(MYUBRR);  //UART초기화 함수 호출
+	sei();				//인터럽트 허용
 
 	while(1)
-
 	{
-
 		//TODO:: Please write your application code
-
-		_delay_ms(20);         //20ms 지연시간
-
-		USART_Transmit(data);  // UART#1 1Byte 전송함수
-
+		if(com1.rcvFlag == 1)
+		{
+			USART_putString(com1.rcvBuff,MAX_RX_NUM);	//수신된 데이터 유아트 전송 (Echo)
+			com1.rcvFlag =0;	//유아트 수신 플래그 클리어
+		}
 	}
-
 	return 0;
-
 }
